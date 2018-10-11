6932371583bf63606f25a17127c1a7345b693324
Jakub Kanclerz <jakub@jkan.pl>
2018-10-11 10:27:33 +0000

    add assign limit to card

---
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -0,0 +1,16 @@
+package pl.jkan.creditcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CreditCardTest {
+    
+    @Test
+    public void allowAssignLimitToCard() {
+        CreditCard card = new CreditCard();
+        
+        card.assignLimit(2000);
+        
+        Assert.assertTrue(card.getLimit() == 2000);
+    }
+}
