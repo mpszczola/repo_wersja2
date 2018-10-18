f0e670b9348dfcc75363bb5d8ca78bb0a95d6bf6
Jakub Kanclerz <jakub@jkan.pl>
2018-10-18 10:21:04 +0000

    describe not enough founds case

--- creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -4,13 +4,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CreditCardTest {
-    
+
     @Test
     public void allowAssignLimitToCard() {
         CreditCard card = new CreditCard();
-        
+
         card.assignLimit(2000);
-        
+
         Assert.assertTrue(card.getLimit() == 2000);
+    }
+
+    @Test
+    public void cantWithdrawWhenCantAfford() {
+
     }
 }
