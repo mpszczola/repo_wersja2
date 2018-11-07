05186f9155d9f8986347c053a5a6583b67528a9f
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2018-11-07 23:50:44 +0100

    extract money creation, hide a little technical type

--- creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -8,33 +8,36 @@ import java.math.BigDecimal;
 import static pl.jkan.creditcard.TestHelper.assertThrows;
 
 public class CreditCardTest {
+    private static BigDecimal money(double x) {
+        return BigDecimal.valueOf(x);
+    }
 
     @Test
     public void allowAssignLimitToCard() {
         CreditCard card1 = new CreditCard();
         CreditCard card2 = new CreditCard();
-        card1.assignLimit(BigDecimal.valueOf(2000));
-        card2.assignLimit(BigDecimal.valueOf(2000));
-        Assert.assertTrue(card1.getLimit().equals(BigDecimal.valueOf(2000)));
-        Assert.assertTrue(card2.getLimit().equals(BigDecimal.valueOf(2000)));
+        card1.assignLimit(money(2000));
+        card2.assignLimit(money(2000));
+        Assert.assertTrue(card1.getLimit().equals(money(2000)));
+        Assert.assertTrue(card2.getLimit().equals(money(2000)));
     }
 
     @Test(expected = NotEnoughMoneyException.class)
     public void cantWithdrawWhenCantAfford() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(2000));
-        card.withdraw(BigDecimal.valueOf(3000));
-        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(2000)));
+        card.assignLimit(money(2000));
+        card.withdraw(money(3000));
+        Assert.assertTrue(card.getLimit().equals(money(2000)));
     }
 
     @Test
     public void cantWithdrawWhenCantRunOutOfMoney() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(2000));
-        card.withdraw(BigDecimal.valueOf(1500));
+        card.assignLimit(money(2000));
+        card.withdraw(money(1500));
 
         try {
-            card.withdraw(BigDecimal.valueOf(1000));
+            card.withdraw(money(1000));
             Assert.fail("Should throw exception");
         } catch (NotEnoughMoneyException e){
             Assert.assertTrue(true);
@@ -52,57 +55,57 @@ public class CreditCardTest {
     @Test(expected = NotEnoughMoneyException.class)
     public void cantWithdrawWhenWhenOverTheLimit() throws Exception{
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(200));
-        card.withdraw(BigDecimal.valueOf(300));
+        card.assignLimit(money(200));
+        card.withdraw(money(300));
     }
 
     @Test(expected = LimitAlreadyAssignedException.class)
     public void limitCantBeAssignTwice() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(200));
-        card.assignLimit(BigDecimal.valueOf(300));
+        card.assignLimit(money(200));
+        card.assignLimit(money(300));
     }
 
     @Test(expected = InsufficientCreditLimitException.class)
     public void limitMustBeGreaterThan0() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(0));
+        card.assignLimit(money(0));
     }
 
     @Test(expected = InsufficientCreditLimitException.class)
     public void limitMustBePositive() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(-100));
+        card.assignLimit(money(-100));
     }
 
     @Test(expected = TransactionOnBlockedCardException.class)
     public void cantWithdrawFromBlocked() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(100));
+        card.assignLimit(money(100));
         card.block();
-        card.withdraw(BigDecimal.valueOf(50));
+        card.withdraw(money(50));
     }
 
     @Test
     public void repayCredit() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(1000));
+        card.assignLimit(money(1000));
 
-        card.withdraw(BigDecimal.valueOf(500));
-        card.withdraw(BigDecimal.valueOf(200));
-        card.repay(BigDecimal.valueOf(400));
+        card.withdraw(money(500));
+        card.withdraw(money(200));
+        card.repay(money(400));
 
-        Assert.assertTrue(BigDecimal.valueOf(700).equals(card.getBalance()));
+        Assert.assertTrue(money(700).equals(card.getBalance()));
     }
 
     @Test
     public void itIsNotPossibleToRepayNegativeAmount() {
         CreditCard card = new CreditCard();
-        card.assignLimit(BigDecimal.valueOf(1000));
+        card.assignLimit(money(1000));
 
         assertThrows(
                 CantRepayNegativeAmountException.class,
-                () -> card.repay(BigDecimal.valueOf(-200))
+                () -> card.repay(money(-200))
         );
     }
 }
